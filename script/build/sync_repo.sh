682522867d6a06c6c5436a7573938e84198196df
Martin Gruner <mg@zammad.com>
2022-03-01 08:36:38 +0100

    Fix github sync.

--- script/build/sync_repo.sh
+++ script/build/sync_repo.sh
@@ -21,14 +21,13 @@ fi
 
 git checkout $CI_BUILD_REF_NAME
 if [ "$CI_BUILD_REF_NAME" != "$CI_BUILD_TAG" ]; then
-  git pull --rebase origin $CI_BUILD_REF_NAME
+  git reset --hard origin/$CI_BUILD_REF_NAME
 fi
 
 if git remote | grep github > /dev/null; then
   git remote rm github
 fi
 git remote add github $GITHUB_DEST
-git fetch github
 
 if [ "$CI_BUILD_REF_NAME" != "$CI_BUILD_TAG" ]; then
   git push github $CI_BUILD_REF_NAME
